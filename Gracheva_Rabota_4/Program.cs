250a907d52f2f677a5f75150d3b4cafa3656e2ae
grrachevva <144918879+grrachevva@users.noreply.github.com>
2024-01-09 15:07:44 +0300

    Add files via upload

---
+++ Gracheva_Rabota_4/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace zadanie4
+{
+ class Program
+ {
+  static void Main(string[] args)
+  {
+   double x1, x2, x3, y1, y2, y3;
+   double eps = 0.0001;
+   bool FlgAccess = true;
+   Console.WriteLine("Введите координаты первой точки (x1, y1):");
+   FlgAccess = double.TryParse(Console.ReadLine(), out x1);
+   FlgAccess = double.TryParse(Console.ReadLine(), out y1);
+
+   Console.WriteLine("Введите координаты второй точки (x2, y2):");
+   FlgAccess = double.TryParse(Console.ReadLine(), out x2);
+   FlgAccess = double.TryParse(Console.ReadLine(), out y2);
+
+   Console.WriteLine("Введите координаты третьей точки (x3, y3):");
+   FlgAccess = double.TryParse(Console.ReadLine(), out x3);
+   FlgAccess = double.TryParse(Console.ReadLine(), out y3);
+
+   if (FlgAccess)
+   {
+    if (Math.Abs((y3 - y2) * (x2 - x1) - (y2 - y1) * (x3 - x2)) < eps)
+    {
+     Console.WriteLine("Точки лежат на одной прямой");
+     Console.ReadKey(true);
+    }
+    else
+    {
+     Console.WriteLine("Точки НЕ лежат на одной прямой");
+     Console.ReadKey(true);
+    }
+   }
+   else
+   {
+    Console.WriteLine("Введены некорректные данные");
+    Console.ReadKey(true);
+   }
+  }
+ }
+}
